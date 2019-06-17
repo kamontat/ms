d3a3037a703c50f1f4be9782ed5d5f01e1856299
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-06-17 10:31:55 +0700

    fix(typo): power management is too long

--- src/mac/power_management.sh
+++ src/mac/power_management.sh
@@ -19,33 +19,33 @@ __ms__load_power_management_config() {
 }
 
 __ms__load_power_management_settings() {
-  __ms_log "Mac power management" "Starting..."
+  __ms_log "Mac power" "Starting..."
 
   if is_variable_exist "$_MS_PM_ENABLE_STANDBY"; then
     sudo pmset -a standby "$(__ms_choose_word "$_MS_PM_ENABLE_STANDBY" "1" "0")"
-    __ms_log "Mac power management" "$(__ms_highlight "$(__ms_choose_word "$_MS_PM_ENABLE_STANDBY" "Enable" "Disable")") standby features"
+    __ms_log "Mac power" "$(__ms_highlight "$(__ms_choose_word "$_MS_PM_ENABLE_STANDBY" "Enable" "Disable")") standby features"
 
     if is_variable_exist "$_MS_PM_STANDBY_THRESHOLD"; then
       sudo pmset -a highstandbythreshold "$_MS_PM_STANDBY_THRESHOLD"
-      __ms_log "Mac power management" "threshold of high and low is $(__ms_highlight "${_MS_PM_STANDBY_THRESHOLD}")"
+      __ms_log "Mac power" "threshold of high and low is $(__ms_highlight "${_MS_PM_STANDBY_THRESHOLD}")"
     else
-      __ms_print_skip "Mac power management" "standby threshold"
+      __ms_print_skip "Mac power" "standby threshold"
     fi
 
     if is_variable_exist "$_MS_PM_STANDBY_DELAY_HIGH"; then
       sudo pmset -a standbydelayhigh "$_MS_PM_STANDBY_DELAY_HIGH"
-      __ms_log "Mac power management" "standby delay time is $(__ms_highlight "${_MS_PM_STANDBY_DELAY_HIGH}") seconds (high)"
+      __ms_log "Mac power" "standby delay time is $(__ms_highlight "${_MS_PM_STANDBY_DELAY_HIGH}") seconds (high)"
     else
-      __ms_print_skip "Mac power management" "standby delay (high)"
+      __ms_print_skip "Mac power" "standby delay (high)"
     fi
 
     if is_variable_exist "$_MS_PM_STANDBY_DELAY_LOW"; then
       sudo pmset -a standbydelaylow "$_MS_PM_STANDBY_DELAY_LOW"
-      __ms_log "Mac power management" "standby delay time is $(__ms_highlight "${_MS_PM_STANDBY_DELAY_LOW}") seconds (low)"
+      __ms_log "Mac power" "standby delay time is $(__ms_highlight "${_MS_PM_STANDBY_DELAY_LOW}") seconds (low)"
     else
-      __ms_print_skip "Mac power management" "standby delay (low)"
+      __ms_print_skip "Mac power" "standby delay (low)"
     fi
   else
-    __ms_print_skip "Mac power management" "standby settings"
+    __ms_print_skip "Mac power" "standby settings"
   fi
 }
